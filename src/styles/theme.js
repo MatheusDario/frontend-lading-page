2090b9e4a5ce2e75ba7afe34438e1383d2f18db7
Matheus Dario <dario.matheus97@gmail.com>
2023-12-11 21:08:26 -0300

    created MenuLink

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,17 +2,21 @@ export const theme = {
   colors: {
     white: '#f0f0f2',
     blue: '#023059',
-    lightBlue: '#065473',
-    aquaBlue: '#08748c',
+    lightblue: '#065473',
+    aquablue: '#08748c',
     gren: '#02735e',
     lightGrenn: '#6ab04c',
     lightGray: ' #cecece',
     red: '#dc143c',
   },
   spacing: {
-    smal: '1rem',
-    medium: '2rem',
-    large: '3rem',
+    xmall: '.8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
   },
   font: {
     sizes: {
