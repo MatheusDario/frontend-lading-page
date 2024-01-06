e7a1a380dbd5b2af9f048f8777fa26c6d615934d
Matheus Dario <dario.matheus97@gmail.com>
2024-01-06 16:16:11 -0300

    update url api

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  url: 'http://localhost:1337/api/pages?populate=deep',
+  url: 'https://apilandingpage.matheusdario.com.br/api/pages?populate=deep',
   siteName: 'Landing-Page',
   defaultSlug: 'landing-page',
 };
